c7670f6b8f2d77d06dbe7fcc656de8838d1bf352
Joeumar Souza <joeu@Joeumars-MacBook-Pro.local>
2023-09-05 17:15:07 -0300

    adding config for github actions

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/joeu"
           target="_blank"
           rel="noopener noreferrer"
         >
-          learn react
+          joeu
         </a>
       </header>
     </div>
